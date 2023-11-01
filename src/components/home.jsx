ac83117c1f6b6e707db4eb6876e9f4b104d5317e
amirmoazzam <amirhassan@duck.com>
2023-11-01 23:07:50 +0330

    home entrance completed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,20 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
         {/* container */}
-        <div></div>
+        <div className='max-w-[1080px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-orange-600'>Hello there, welcome to</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6] capitalize'>amir moazzam</h1>
+            <h3 className='text-4xl sm:text-7xl font-bold text-[#8892ba]'>personal website</h3>
+            <p className='text-[#8892ba] py-4 max-w-[700px] capitalize'>i'm react frontend web developer. i'm a self-thought programmer and i started my work from 2020. it's my pleasure to read more about me in my resume.</p>
+            <div>
+                <button className='group text-white font-bold border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-white'>
+                    view work
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <HiArrowNarrowRight className='ml-3'/>
+                    </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
